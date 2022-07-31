39cba18af8b8fff5c05d0f698ee9199b38eb5e99
zonde306 <13820323+zonde306@users.noreply.github.com>
2022-07-31 15:50:54 +0800

    .

--- SVPatch.cs
+++ SVPatch.cs
@@ -32,11 +32,12 @@ namespace RF5_Harem
 					continue;
 
 				// 把陪睡时修改的还原
-				if (data.Home != Define.Place.Police || data.BedPatrolPointName != "00_Police_b7")
+				if (data.Home != Define.Place.Police || data.BedPatrolPointName != "00_Police_b7" || data.ChatTalkLv < 4)
 				{
 					data.Home = Define.Place.Police;
 					data.BedPatrolPointName = PLAYER_BED;
 					data.BedPatrolPoint = NpcPatrolPointManager.Instance.GetPoint(data.BedPatrolPointName).GetComponent<NpcPatrolPoint>();
+					data.SetChatTalkLv(4, true);
 					Main.Log.LogInfo(string.Format("ResetNpcBed npcid:{0}({1}), Home:{2}, Bed:{3}, NickName:{4}, PlayerNickName:{5}",
 						data.NpcId, (Define.NPCID)data.NpcId, data.Home, data.BedPatrolPointName,
 						data.NickNameFromPlayerId, data.NickNameToPlayerId
