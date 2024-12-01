cf6982166e4d6830e059e202bc0b020d48f4bd51
David <davidthemaster30@hotmail.com>
2024-12-01 16:02:13 -0500

    update release script

--- make_release.sh
+++ make_release.sh
@@ -3,4 +3,4 @@ rm -rf bin
 
 dotnet build RF5_Harem.csproj -f net6.0 -c Release
 
-zip -j 'RF5_Harem.1.2.0.zip' './bin/Release/net6.0/RF5_Harem.dll' './bin/Release/net6.0/RF5_Harem.cfg'
+zip -j 'RF5_Harem.1.2.1.zip' './bin/Release/net6.0/RF5_Harem.dll' './bin/Release/net6.0/RF5_Harem.cfg'
